38b7cdfa60eafe10af57e5f4eac1184aa0dc4a97
Yevhenii Zhaivoronok <Zheckan1120@gmail.com>
2025-11-07 02:18:37 +0100

    Replace old home page with a new main page component

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      Hi, I'm main page. Go somewhere else.
+    </div>
+  );
+}
